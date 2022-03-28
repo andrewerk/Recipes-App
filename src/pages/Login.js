db245cc71336367195d26da4e246219b1d12e514
Fábio Sabah <fabiosaba@gmail.com>
2022-03-28 18:18:46 -0300

    page-login-req-1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,26 @@ import React from 'react';
 
 function Login() {
   return (
-    <h1>LoginPage</h1>
+    <>
+      <h1>Login</h1>
+      <input
+        type="text"
+        data-testid="email-input"
+        placeholder="someone@email.com"
+      />
+      <input
+        type="password"
+        data-testid="password-input"
+        placeholder="Password"
+      />
+      <button
+        type="submit"
+        data-testid="login-submit-btn"
+      >
+        Enter
+      </button>
+    </>
+
   );
 }
 
