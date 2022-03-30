7783fdbbdb4af86cbbf4580073fd84b3a53b1a2c
gustavoweiss <gustavoweissformiga@gmail.com>
2022-03-30 16:00:27 -0300

    feat: implemented the routes after  clicking the icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import '../css/Footer.css';
 
 import drinkIcon from '../images/drinkIcon.svg';
@@ -7,7 +7,7 @@ import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 
 function Footer() {
-  // const history = useHistory();
+  const history = useHistory();
   return (
     <footer data-testid="footer">
       <input
@@ -16,19 +16,21 @@ function Footer() {
         type="image"
         src={ drinkIcon }
         alt="drink icon"
-        // onClick={ () => history.push('/') }
+        onClick={ () => history.push('/drinks') }
       />
       <input
         data-testid="explore-bottom-btn"
         type="image"
         src={ exploreIcon }
         alt="explore icon"
+        onClick={ () => history.push('/explore') }
       />
       <input
         data-testid="food-bottom-btn"
         type="image"
         src={ mealIcon }
         alt="meal icon"
+        onClick={ () => history.push('/foods') }
       />
     </footer>
   );
