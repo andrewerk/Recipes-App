e6f4c171992944cf602dad6b3bebbab67c90eefd
gustavoweiss <gustavoweissformiga@gmail.com>
2022-04-03 11:31:43 -0300

    fix:Cleaned the code from tests

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,49 +1,21 @@
 import React, { useState } from 'react';
 import DoneRecipeCard from '../components/DoneRecipeCard';
-// import FilterSavedRecipes from '../components/FilterSavedRecipes';
 import HeaderWithoutSearch from '../components/HeaderWithoutSearch';
 import '../css/Done-recipes.css';
 
 function DoneRecipes() {
-  const doneRecipes = [
-    {
-      id: '52771',
-      type: 'food',
-      nationality: 'Italian',
-      category: 'Vegetarian',
-      alcoholicOrNot: '',
-      name: 'Spicy Arrabiata Penne',
-      image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-      doneDate: '23/06/2020',
-      tags: ['Pasta', 'Curry'],
-    },
-    {
-      id: '178319',
-      type: 'drink',
-      nationality: '',
-      category: 'Cocktail',
-      alcoholicOrNot: 'Alcoholic',
-      name: 'Aquamarine',
-      image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-      doneDate: '23/06/2020',
-      tags: [],
-    },
-  ];
-  localStorage.setItem('doneRecipes', JSON.stringify(doneRecipes));
-  const teste = JSON.parse(localStorage.getItem('doneRecipes'));
-  console.log(teste);
-  const [filteredDoneRecipes, setFilteredDoneRecipes] = useState(teste);
+  const doneRecipesFromLocal = JSON.parse(localStorage.getItem('doneRecipes'));
+  const [filteredDoneRecipes, setFilteredDoneRecipes] = useState(doneRecipesFromLocal);
 
   return (
     <div>
       <HeaderWithoutSearch title="Done Recipes" />
-      {/* <FilterSavedRecipes /> */}
       <div className="btn-container">
         <button
           className="done-recipes-btn"
           data-testid="filter-by-all-btn"
           type="button"
-          onClick={ () => setFilteredDoneRecipes(teste) }
+          onClick={ () => setFilteredDoneRecipes(doneRecipesFromLocal) }
         >
           All
         </button>
